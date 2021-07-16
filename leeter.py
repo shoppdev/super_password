71dfac66bd02f2867a68e4dc0a246249519593bf
shoppdev <tegbert@gmail.com>
2021-07-15 23:50:43 -0600

    workin on it

--- leeter.py
+++ leeter.py
@@ -1,27 +1,12 @@
+import random
+import pass_creator
 
-
-def check_letter(letter):
-    ascii_num = int(ord(letter))
-
-    if 65 <= ascii_num <= 95:
-        print('big letter')
-    elif 97 <= ascii_num <= 122:
-        print('little letter')
-    elif 48 <= ascii_num <= 57:
-        print('number')
-    else:
-        print('symbol')
-
-    # get letter
-    # get ascii code
-    # print(ord(letter))
-    # modify letter based on ascii
-
-
-def leet_it(word):
-    for letter in word:
-        check_letter(letter)
+def bookend(word):
+    word = pass_creator.random_symbol() + word
+    word = pass_creator.random_number() + word
+    word = pass_creator.random_letter() + word
+    return word
 
 
 if __name__ == '__main__':
-    leet_it('p2Kh57$Ujk')
+    
